037aea4f8d4caae95b7727477abe72fca5f70884
WolfFer22 <lfof.ferreira@gmail.com>
2022-04-28 12:06:53 +0100

    Part 3 Finalized

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,10 +1,16 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here
-        System.out.println("Once upon a time");
-        System.out.println("there was");
-        System.out.println("a dinosaur");
+        
+        Scanner scanner = new Scanner(System.in);        
+        ArrayList<Integer> list = new ArrayList<>(); 
+        
+        
+        
     }
 }
--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class PersonalDetails {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);      
+                
         int sumYears = 0;
         int countYears = 0;
         String longestName = "";        
@@ -38,3 +38,31 @@ public class PersonalDetails {
         
     }
 }
+//            Scanner scanner = new Scanner(System.in);
+//
+//            ArrayList<String> names = new ArrayList<>();
+//            ArrayList<Integer> birthYears = new ArrayList<>();
+//            while (true) {
+//                String input = scanner.nextLine();
+//                if (input.equals("")) {
+//                    break;
+//                }
+//
+//                String[] parts = input.split(",");
+//                names.add(parts[0]);
+//                birthYears.add(Integer.valueOf(parts[1]));
+//            }
+//
+//            String longest = names.get(0);
+//            for (String name : names) {
+//                if(name.length() > longest.length()) {
+//                    longest = name;
+//                }
+//            }
+//            System.out.println("Longest name: " + longest);
+//
+//            int sumOfBirthYears = 0;
+//            for (int year : birthYears) {
+//                sumOfBirthYears = sumOfBirthYears + year;
+//            }
+//            System.out.println("Average of the birth years: " + 1.0 * sumOfBirthYears / birthYears.size());
